6c814edb1fd53b0ff6270138b182fd7e8d7e6d65
sharmamayank221 <sharmamayank221@gmail.com>
2023-04-03 09:30:04 +0545

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
       </div>
 
       <div className="flex items-center">
-        <div className={`mr-6 h-[27px] w-[27px] rounded-full bg-white`}>
+        <div className={`mr-5 h-[27px] w-[27px] rounded-full bg-white`}>
           <h3 className="flex items-center justify-center font-Lora text-xl text-black -mt-0.5">
             0
           </h3>
@@ -23,9 +23,30 @@ export default function Footer() {
         <Image src="/hand.png" alt="finger-numbers" width={135} height={180} />
         <span className="mr-4 font-Lora text-xl text-white">Left hand</span>
       </div>
-      <span className="mr-4 font-Lora text-xl text-white w-1/2">
+      <span className="mr-4 font-Lora text-xl text-white w-[25%]">
         Standard tuning: E2,A2,D3,G3,B3,E4
       </span>
+      <div className="mr-4 font-Lora text-xl text-white w-1/2 flex">
+        Stirngs:
+        <div className="flex items-center ">
+          <span className="text-[#38DBE5] ">6th String </span>
+        </div>
+        <div className="flex items-center ml-3">
+          <span className="text-[#C65151] ">5th String </span>
+        </div>
+        <div className="flex items-center ml-3">
+          <span className="text-[#C2D934] ">4th String </span>
+        </div>
+        <div className="flex items-center ml-3">
+          <span className="text-[#F642EF] ">3rd String </span>
+        </div>
+        <div className="flex items-center ml-3">
+          <span className="text-[#EA9E2D] ">2nd String </span>
+        </div>
+        <div className="flex items-center ml-3">
+          <span className="">1st String </span>
+        </div>
+      </div>
     </div>
   );
 }
