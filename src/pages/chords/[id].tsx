03aed2239d8f90c6a35b3acf21139bc7ca26c41c
sharmamayank221 <sharmamayank221@gmail.com>
2023-04-18 08:16:27 +0545

    add spinner

--- src/pages/chords/[id].tsx
+++ src/pages/chords/[id].tsx
@@ -6,18 +6,109 @@ import { ParsedUrlQuery } from "querystring";
 import ChordName from "@/components/ChordName";
 import { A } from "@/types/chord.types";
 import getIDs from "@/helpers/getIDs";
+import { useRouter } from "next/router";
 
 interface IParams extends ParsedUrlQuery {
   slug: string;
 }
 
 export default function Chord(props: any) {
+  const router = useRouter();
   const { id } = props;
   const { chordData } = props;
 
   const DynamicComponentWithNoSSR = dynamic(
     () => import("../../components/Guitar")
   );
+
+  if (router.isFallback) {
+    return (
+      <div
+        aria-label="Loading..."
+        role="status"
+        className="flex items-center space-x-2"
+      >
+        <svg
+          className="h-6 w-6 animate-spin stroke-white"
+          viewBox="0 0 256 256"
+        >
+          <line
+            x1="128"
+            y1="32"
+            x2="128"
+            y2="64"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="24"
+          ></line>
+          <line
+            x1="195.9"
+            y1="60.1"
+            x2="173.3"
+            y2="82.7"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="24"
+          ></line>
+          <line
+            x1="224"
+            y1="128"
+            x2="192"
+            y2="128"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="24"
+          ></line>
+          <line
+            x1="195.9"
+            y1="195.9"
+            x2="173.3"
+            y2="173.3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="24"
+          ></line>
+          <line
+            x1="128"
+            y1="224"
+            x2="128"
+            y2="192"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="24"
+          ></line>
+          <line
+            x1="60.1"
+            y1="195.9"
+            x2="82.7"
+            y2="173.3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="24"
+          ></line>
+          <line
+            x1="32"
+            y1="128"
+            x2="64"
+            y2="128"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="24"
+          ></line>
+          <line
+            x1="60.1"
+            y1="60.1"
+            x2="82.7"
+            y2="82.7"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="24"
+          ></line>
+        </svg>
+        <span className="text-xs font-medium text-white">Loading...</span>
+      </div>
+    );
+  }
 
   return (
     <div className="h-full w-full overflow-hidden bg-black ">
