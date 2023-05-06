8d1311644eed10f21018a02d5fb1c1ab8b5fd546
sharmamayank221 <sharmamayank221@gmail.com>
2023-05-06 10:26:47 +0545

    add contact and seo description

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,11 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="bg-black">
-      <div className="container mx-auto flex items-center justify-between "></div>
+      <div className="container mx-auto flex items-center justify-center text-white">
+        <a href="mailto:chordsofguitar@gmail.com" target="_blank">
+          chordsofguitar.gmail.com
+        </a>
+      </div>
     </div>
   );
 }
